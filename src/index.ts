507d7a4e534a993e10208411777495c7fd6b6ca5
MURAKAMI Masahiko <fossamagna2@gmail.com>
2021-10-19 23:15:33 +0900

    Use TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,16 @@
-const path = require('path');
+import { $TSAny, $TSContext } from 'amplify-cli-core';
+import * as path from 'path';
 
-async function executeAmplifyCommand(context) {
+export async function executeAmplifyCommand(context: $TSContext) {
   const commandsDirPath = path.normalize(path.join(__dirname, 'commands'));
   const commandPath = path.join(commandsDirPath, context.input.command);
   const commandModule = require(commandPath);
   await commandModule.run(context);
 }
 
-async function handleAmplifyEvent(context, args) {
+export async function handleAmplifyEvent(context: $TSContext, args: $TSAny) {
   const eventHandlersDirPath = path.normalize(path.join(__dirname, 'event-handlers'));
   const eventHandlerPath = path.join(eventHandlersDirPath, `handle-${args.event}`);
   const eventHandlerModule = require(eventHandlerPath);
   await eventHandlerModule.run(context, args);
 }
-
-module.exports = {
-  executeAmplifyCommand,
-  handleAmplifyEvent,
-};
